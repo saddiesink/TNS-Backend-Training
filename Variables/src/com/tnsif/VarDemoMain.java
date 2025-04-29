d3455068d57efce72f290bcbc3761ec47caa93c5
varni <varni@VarnithaReddy.bbrouter>
2025-04-29 18:30:32 +0530

    Variables

---
+++ Variables/src/com/tnsif/VarDemoMain.java
@@ -0,0 +1,20 @@
+package com.tnsif;
+
+public class VarDemoMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DemoTypeVar ob=new DemoTypeVar();
+		ob.sid=6719;
+		ob.sname="varnitha";
+		ob.display();
+		DemoTypeVar o=new DemoTypeVar();
+		o.sid=6718;
+		o.sname="akhila";
+		o.display();
+		
+		
+
+	}
+
+}
