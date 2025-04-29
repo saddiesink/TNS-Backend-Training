a0df55cbfaa32959a0564015d34e4c10c179f162
varni <varni@VarnithaReddy.bbrouter>
2025-04-29 18:35:24 +0530

    add

---
+++ Variables/src/com/tnsif/DemoTypeVar.java
@@ -0,0 +1,11 @@
+package com.tnsif;
+
+public class DemoTypeVar {
+	int sid;
+	String sname;
+	static String collegename="sriindu";
+	void display() {
+		System.out.println("The details are "+ sid +" "+sname+" "+collegename);
+	}
+
+}
